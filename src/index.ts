cc02f349eb761f77d21d4734de3b5502c0d041ab
Nikolay Dymura <nikolay.dymura@gmail.com>
2022-08-18 20:12:13 +0300

    Fixed versions

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,14 @@ admin.initializeApp({credential: admin.credential.applicationDefault()});
 
 const app: express.Express = express();
 
+app.use((req,res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://first-back-end-21211.web.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 app.use('/', (req, res) => {
   res.send({version: `1.0.${process.env.GITHUB_RUN_ID}-${process.env.GITHUB_SHA}`});
 });
@@ -15,9 +23,5 @@ app.use('/', (req, res) => {
 app.use('/signup/info', new UsersController(
     new UsersRepository(admin.firestore())).build());
 
-app.use((req,res, next) => {
-  res.set('Access-Control-Allow-Origin', '*');//'https://first-back-end-21211.web.app');
-  next();
-});
 
 exports.api = functions.https.onRequest(app);

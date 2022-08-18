1314968b6abf4ae34a9fba1bb115810385141e44
Nikolay Dymura <nikolay.dymura@gmail.com>
2022-08-18 19:52:23 +0300

    Fixed CORS

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,15 @@ admin.initializeApp({credential: admin.credential.applicationDefault()});
 
 const app: express.Express = express();
 
-app.use('/', (req, res) =>{
-  res.send({version: '1.0.0'});
+app.use('/', (req, res) => {
+  res.send({version: `1.0.${process.env.GITHUB_RUN_ID}-${process.env.GITHUB_SHA}`});
 });
 
 app.use('/signup/info', new UsersController(
     new UsersRepository(admin.firestore())).build());
 
 app.use((req,res, next) => {
-  res.set('Access-Control-Allow-Origin', 'https://first-back-end-21211.web.app');
+  res.set('Access-Control-Allow-Origin', '*');//'https://first-back-end-21211.web.app');
   next();
 });
 
